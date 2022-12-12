65f36f9963ebe74000b3bc2ff47bb220ab46ebf5
J. Fernandes <j.fernandes8077@gmail.com>
2022-12-11 21:59:52 -0300

    fix: reafactor with context api and add auto login

--- src/components/FormLogin/shemaLogin.js
+++ src/components/FormLogin/shemaLogin.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const formSchemaLogin = yup.object().shape({
-  email: yup.string().required("Campo obrigatório"),
+  email: yup.string().required("Campo obrigatório").email(),
   password: yup.string().required("Campo obrigatório"),
 });
